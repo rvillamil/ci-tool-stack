d0f429bb20c8767022ef19ab4fb5066659b86ed0
Rodrigo <rodrigo.villamil@gmail.com>
2017-03-20 19:39:33 +0100

    First big commit

---
+++ tools.sh
@@ -0,0 +1,259 @@
+#!/bin/bash
+#
+# Copyright (C) Rodrigo Villamil Perez 2016
+# Fichero: tools.sh
+# Autor: Rodrigo Villamil Perez
+# Fecha: 05/11/2016
+#
+# Description: Utilities for C.I. tools maintenance
+#
+
+# Log File..
+LOG_FILE="${0%.*}.log"
+source ./utils/functions-docker.sh
+
+# Images and containers name
+JENKINS_IMAGE="citoolstack_img-jenkins-master"
+JENKINS_MASTER_CONTAINER="cnt-jenkins-master"
+
+JENKINS_DATA_IMAGE="citoolstack_img-jenkins-master-data"
+JENKINS_DATA_CONTAINER="cnt-jenkins-master-data"
+
+SONAR_IMAGE="citoolstack_img-sonarqube"
+SONAR_CONTAINER="cnt-sonarqube"
+
+POSTGRES_IMAGE="citoolstack_img-postgres"
+POSTGRES_CONTAINER="cnt-postgres"
+
+POSTGRES_DATA_IMAGE="citoolstack_img-postgres-data"
+POSTGRES_DATA_CONTAINER="cnt-postgres-data"
+
+# Images and containers list
+APP_IMAGES="${JENKINS_IMAGE} ${SONAR_IMAGE} ${POSTGRES_IMAGE}"
+APP_CONTAINERS="${JENKINS_MASTER_CONTAINER} ${SONAR_CONTAINER} ${POSTGRES_CONTAINER}"
+
+DATA_IMAGES="${JENKINS_DATA_IMAGE} ${POSTGRES_DATA_IMAGE}"
+DATA_CONTAINERS="${JENKINS_DATA_CONTAINER} ${POSTGRES_DATA_CONTAINER}"
+
+ALL_IMAGES="${APP_IMAGES} ${DATA_IMAGES}"
+ALL_CONTAINERS="${APP_CONTAINERS} ${DATA_CONTAINERS}"
+
+# Other environment variables
+JENKINS_HOST_CONNECTION_STRING="rvp001es:rvpquality@localhost:8080"
+JENKINS_HOME="/var/jenkins_home"
+JENKINS_VOLUME_LOG="/var/log/jenkins"
+BACKUP_JENKINS_DIRECTORY="$(pwd)/backup-data/cnt-jenkins-master-data"
+BACKUP_POSTGRES_DIRECTORY="$(pwd)/backup-data/cnt-postgres-data"
+
+PLUGINS_FILEPATH="$(pwd)/jenkins/jenkins-master/conf/plugins.txt"
+POSTGRES_PGDATA="/var/lib/postgresql/data/pgdata"
+DOCKER_COMPOSE_FILE="docker-compose.yml"
+# ---------------------------- Docker utilities --------------------------------
+run_all()
+{
+    docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
+    #docker exec ${JENKINS_MASTER_CONTAINER} ps -ef | grep java
+	message "\n- C.I services running in background (-d) !! . You can see the compose file in '${DOCKER_COMPOSE_FILE}'"
+	message "- Jenkins at: http://localhost:8383'"
+	message "- SonarQube at: http://localhost:9000'"
+}
+
+stop_all()
+{
+	docker-compose -f ${DOCKER_COMPOSE_FILE} stop
+    message "\n- C.I services are being stopped.."
+}
+backup_jenkins_data_container()
+{
+    volumes_to_backup="${JENKINS_HOME} ${JENKINS_VOLUME_LOG}"
+	backup_file_name="${JENKINS_DATA_CONTAINER}.tar.gz" 
+    backup_file_full_path="${BACKUP_JENKINS_DIRECTORY}/${backup_file_name}"
+	if [ -f ${backup_file_full_path} ];then
+		message "ERROR! The backup file '${backup_file_full_path}' already exists!. Delete it before" 
+		return 1
+	fi
+    # Recuperamos los plugins por si acaso..
+    ! get_plugins_from_jenkins && return 1
+    # Tiramos los contenedores ...
+    stop_all_compose_containers
+    docker ps
+    backup_data_volume_container ${JENKINS_DATA_CONTAINER} ${BACKUP_JENKINS_DIRECTORY} ${backup_file_name} ${JENKINS_DATA_IMAGE} ${volumes_to_backup}
+    message "\nBackup ready! ${?}"
+}
+
+restore_jenkins_data_container()
+{
+    volumes_to_restore="${JENKINS_HOME} ${JENKINS_VOLUME_LOG}"
+    backup_file_name="${JENKINS_DATA_CONTAINER}.tar.gz" 
+    backup_file_full_path="${BACKUP_JENKINS_DIRECTORY}/${backup_file_name}"  
+    if [ ! -f ${backup_file_full_path} ];then
+		message "ERROR! The backup file to restore '${backup_file_full_path}' not exists!"
+		return 1
+    fi
+	message "Re-creating data container ${JENKINS_DATA_CONTAINER} (if not exist)"
+	docker-compose up "${JENKINS_DATA_IMAGE}" &> ${LOG_FILE} # De esta manera re-creamos el contenedor con la imagen si no existe
+    stop_all_compose_containers
+    docker ps
+    restore_data_volume_container ${JENKINS_DATA_CONTAINER} ${BACKUP_JENKINS_DIRECTORY} ${backup_file_name} ${JENKINS_DATA_IMAGE} ${volumes_to_restore} &> ${LOG_FILE}
+    message "\nRestore ready! ${?}"
+} 
+
+backup_postgres_data_container()
+{
+    volumes_to_backup="${POSTGRES_PGDATA}"
+    backup_file_name="${POSTGRES_DATA_CONTAINER}.tar.gz" 
+    backup_file_full_path="${BACKUP_POSTGRES_DIRECTORY}/${backup_file_name}"
+    if [ -f ${backup_file_full_path} ];then
+		message "ERROR! The backup file '${backup_file_full_path}' already exists!. Delete it before" 
+		return 1
+	fi
+    stop_all_compose_containers
+    docker ps
+    backup_data_volume_container ${POSTGRES_DATA_CONTAINER} ${BACKUP_POSTGRES_DIRECTORY} ${backup_file_name} ${POSTGRES_DATA_IMAGE} ${volumes_to_backup}
+    message "\nBackup ready! ${?}"
+}
+
+restore_postgres_data_container()
+{
+    volumes_to_restore="${POSTGRES_DATA}"
+    backup_file_name="${POSTGRES_DATA_CONTAINER}.tar.gz" 
+    backup_file_full_path="${BACKUP_POSTGRES_DIRECTORY}/${backup_file_name}"  
+    if [ ! -f ${backup_file_full_path} ];then
+		message "ERROR! The backup file to restore ${backup_file_full_path}! not exists!"
+		return 1
+    fi
+ 	message "Re-creating data container ${JENKINS_DATA_CONTAINER} (if not exist)"
+    docker-compose up "${POSTGRES_DATA_IMAGE}" &> ${LOG_FILE} # De esta manera creamos el contenedor con la imagen si no existe
+    stop_all_compose_containers
+	docker ps
+    restore_data_volume_container ${POSTGRES_DATA_CONTAINER} ${BACKUP_POSTGRES_DIRECTORY} ${backup_file_name} ${POSTGRES_DATA_IMAGE} ${volumes_to_restore} &> ${LOG_FILE}
+    message "\nRestore ready! ${?}"
+}
+
+bash_jenkins_master()
+{
+	bash_in_container ${JENKINS_MASTER_CONTAINER}
+}
+
+inspect_jenkins_master()
+{
+	inspect_container ${JENKINS_MASTER_CONTAINER}
+}
+
+clean_all_ci_docker()
+{
+    message "Cleaning C.I IMAGES '${ALL_IMAGES}' and C.I CONTAINERS '${ALL_CONTAINERS}'"
+    docker-compose down  &> ${LOG_FILE}
+    docker rm ${ALL_CONTAINERS} &> ${LOG_FILE}
+    docker rmi ${ALL_IMAGES}  &> ${LOG_FILE}
+    clean_all_dangling_volumes_and_images  
+    message "\nImage list .."    
+    docker images
+    message "\nContainer list .."    
+    docker ps
+}
+
+get_plugins_from_jenkins()
+{
+	message "Getting 'plugins.txt' file from ${JENKINS_HOST_CONNECTION_STRING}"
+    # install plugins; the plugins.txt file can be exported from Jenkins like this:
+    # jenkins_host=username:password@myhost.com:port
+    curl -sSL "${JENKINS_HOST_CONNECTION_STRING}/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/' > /tmp/jenkis_plugins.list
+
+    if [ -s /tmp/jenkis_plugins.list ];then # Si existe y no esta vacio
+		cat /tmp/jenkis_plugins.list > ${PLUGINS_FILEPATH}
+		num_plugins=$(cat ${PLUGINS_FILEPATH} | wc -l | tr -d '[:space:]')
+		cat ${PLUGINS_FILEPATH} >> ${LOG_FILE}
+		message "\n\nThe jenkins plugins file contains '$num_plugins' plugins"
+		message "The file '${PLUGINS_FILEPATH}'has been updated"
+    else
+		message "ERROR! Jenkins '${JENKINS_HOST_CONNECTION_STRING}' is not running!"
+		return 1
+    fi    
+}
+
+get_log_file()
+{
+    docker exec ${JENKINS_MASTER_CONTAINER} cat ${JENKINS_VOLUME_LOG}/jenkins.log
+}
+
+show_jenkins_initial_admin_password()
+{
+    jenkins_secret_key="/var/jenkins_home/secrets/initialAdminPassword"
+	message "Jenkins Secret Key from file ${jenkins_secret_key}"
+    docker exec ${JENKINS_MASTER_CONTAINER} cat ${jenkins_secret_key}
+}
+
+run_svnserver()
+{
+    message "Running mamohr/subversion-edge ..."
+    docker run -p 3343:3343 -p 4434:4434 -p 18080:18080  --name svn-server mamohr/subversion-edge --hostname subversion-server
+    if [ ! $? -eq 0 ];then
+		docker start svn-server
+    fi
+    message "\n Subversion from Collabnet initializing...(1 minute). The image is exposing the data dir of csvn as a volume under '/opt/csvn/data'"
+    message "\n   * URL: http://localhost:3343/csvn/long/auth"
+    message "\n   * Download TEST project 'svn co http://localhost:18080/svn/test test --username=admin'"
+    message "\n   * The username is 'admin' and the Password is 'admin'"
+}
+
+# ----------------------------------------------------------------------
+while :
+do
+    clear
+    cat<<EOF
+ ======================================
+    	C.I Tool Stack Utilities
+ ======================================
+
+	(0) Run C.I. tool stack demonized (docker-compose up -d)
+	(1) Stop C.I tool stack (docker-compose stop)
+	(2) Remove all containers (volumes included) and images from C.I tool stack!
+     
+	(3) JENKINS : 'Backup' Jenkins volume data container (JENKINS_HOME and Log file)
+	(4) JENKINS : 'Restore' Jenkins volume data container
+	(5) JENKINS : Run 'bash' terminal
+	(6) JENKINS : Inspect container
+	(7) JENKINS : Update 'plugins.txt' file with current installed plugins
+	(8) JENKINS : Get file '/var/log/jenkins/jenkins.log'
+	(9) JENKINS : Show secret Jenkins instalation key
+   
+	(a) SONAR : 'Backup' Sonarqube volume data container (Postgres ddbb)
+	(b) SONAR : 'Restore' Sonarqube volume data container (Postgres ddbb)
+    
+	(x) Clean all Volumes and Dangling Images
+	(z) SUBVERSION: Create subversion for testing pruposes in http://localhost:3343/csvn/long/auth
+     
+	(q) Quit
+  --------------------------------
+EOF
+	echo -n "Choose one option: "
+    read -n1 -s
+    echo ""
+    case "$REPLY" in
+	"0")  run_all; exit 0 ;;
+    "1")  stop_all; exit 0 ;;
+	"2")  clean_all_ci_docker; exit 0 ;;
+   
+    "3")  backup_jenkins_data_container ; exit 0 ;;
+    "4")  restore_jenkins_data_container; exit 0 ;;
+    "5")  bash_jenkins_master; exit 0 ;;
+    "6")  inspect_jenkins_master; exit 0 ;;
+    "7")  get_plugins_from_jenkins ; exit 0 ;;
+    "8")  get_log_file jenkins.log ; exit 0 ;;
+    "9")  show_jenkins_initial_admin_password ; exit 0 ;;
+    
+    "a"|"A")  backup_postgres_data_container ; exit 0 ;;
+    "b"|"B")  restore_postgres_data_container; exit 0 ;;
+    
+    "x"|"X")  clean_all_dangling_volumes_and_images ;;
+    "z"|"Z")  run_svnserver; exit 0 ;;
+    
+    "q"|"Q")  exit 0 ;;
+    * )  echo "Invalid option !!" ;;
+    esac
+    echo ""
+    echo -n "Press enter key ..."
+    read enter_key
+done
+
---
+++ utils/functions-docker.sh
@@ -0,0 +1,191 @@
+#!/bin/bash 
+#
+# Copyright (C) Rodrigo Villamil Perez 2016
+# Fichero: functions-docker.sh
+# Autor: Rodrigo Villamil Perez
+# Fecha: 14/11/16
+#
+# Description: Common utilities
+#
+
+# ------------------------- Logger functions -----------------------------------
+# We can set the Log file from external script
+if [ -z "${LOG_FILE}" ];then
+    LOG_FILE="default.log"
+fi
+
+echo -e "WARNING! The log file is ${LOG_FILE}"
+
+log()
+{
+	echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${1}" >> ${LOG_FILE} 
+}
+logInfo()
+{
+    log "[INFO]: ${1}"
+}
+logError()
+{
+    log "[ERROR]: ${1}"
+}
+message()
+{
+	echo -e "${1}"
+}
+
+# ----------------------------- Other functions --------------------------------
+is_empty_dir()
+{
+    [ "$(ls -A ${1})" ] && return 1 || return 0
+}
+
+##
+## copy-paste from https://gist.github.com/DinoChiesa/3e3c3866b51290f31243
+##
+parse_yaml2() {
+    local prefix=$2
+    local s
+    local w
+    local fs
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_]*'
+    fs="$(echo @|tr @ '\034')"
+    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
+    awk -F"$fs" '{
+      indent = length($1)/2;
+      if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+              vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+              printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+      }
+    }' | sed 's/_=/+=/g'
+}
+# ---------------------------- Docker utilities --------------------------------
+# Thank's to:
+#  - https://twasink.net/2016/08/01/setting-up-a-jenkins-server-with-docker/
+#  - https://goo.gl/YDJ088
+#  - https://clusterhq.com/2016/08/15/jenkins-flocker-part1/
+#
+#  - http://www.tricksofthetrades.net/2016/03/14/docker-data-volumes/
+#  - https://goo.gl/NMQZBq
+up_all_compose_containers()
+{
+    message "Builds, (re)creates, starts, and attaches to containers for a service. (docker-compose up) ...."
+    docker-compose up &> ${LOG_FILE} 
+}
+
+stop_all_compose_containers()
+{
+    message "Stop running containers without removing them (docker-compose stop) ..."
+    docker-compose stop &> ${LOG_FILE} 
+}
+
+clean_all_dangling_volumes()
+{
+    message "Cleaning all dangling volumes .."
+    docker volume rm $(docker volume ls -f dangling=true -q)
+}
+
+clean_all_stopped_containers()
+{
+    message "Cleaning all stopped containers...( docker rm $(docker ps -a -q) )"    
+    docker rm $(docker ps -a -q)
+}
+
+clean_all_untagged_containers()
+{
+    message "Cleaning all untagged containers ..(<none>)"
+    docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+}
+
+clean_all_dangling_volumes_and_images()
+{
+    clean_all_dangling_volumes
+    clean_all_untagged_containers
+}
+
+bash_in_container()
+{
+    message "Running bash over ${1}"
+    docker exec -i -t ${1} /bin/bash
+}
+
+inspect_container()
+{
+    message "Inspecting ${1}"
+    docker inspect ${1}
+}
+
+
+#
+#  Backup data volume container
+#  https://docs.docker.com/engine/tutorials/dockervolumes/#backup-restore-or-migrate-data-volumes
+#
+# ${1} : Data volume container name to backup
+# ${2} : Backup directory in local host. Create it, if not exist  
+# ${3} : Backup ".tar.gz" file name
+# ${4} : Image base for creating the backup (debian, fedora, jenkins...)
+# ${5} : Volume list to backup
+#        e.g.: /var/jenkins_home /var/log/jenkins
+#
+backup_data_volume_container()
+{
+    data_container=${1}
+    backup_directory=${2}
+    backup_file_name=${3}
+    image_base=${4}
+    volumes_to_backup=${@:5}
+    backup_file_full_path="${backup_directory}/${backup_file_name}"
+   
+    message "Volumes to backup '${volumes_to_backup}' from data container name as '${data_container}'"
+    docker run --rm  -v ${backup_directory}:/backup --volumes-from ${data_container} ${image_base} tar -Pczvf /backup/${backup_file_name} ${volumes_to_backup}
+    #
+    # -P option evita el problema que me quita la / incial y luego no restaura bien el backup
+    #
+    if [ ${?} -eq 0 ];then
+		message "New backup in '${backup_file_full_path}'"
+		return 0
+    else
+		message "ERROR! creating Backup. Deleting file '${backup_file_full_path}'"
+		rm -r "${backup_file_full_path}"
+		return 1
+    fi
+}
+
+#
+#  Restore the backup data volume container
+#  https://docs.docker.com/engine/tutorials/dockervolumes/#backup-restore-or-migrate-data-volumes
+#
+# ${1} : Data volume container name to restore
+# ${2} : Backup directory in local host. 
+# ${3} : Backup ".tar.gz" file name
+# ${4} : Image base for creating the backup (debian, fedora, jenkins...)
+# ${5} : Volume list to restore
+#        e.g.: /var/jenkins_home /var/log/jenkins
+#
+restore_data_volume_container()
+{
+    data_container=${1}
+    backup_directory=${2}
+    backup_file_name=${3}
+    image_base=${4}
+    volumes_to_restore=${@:5}
+
+    backup_file_full_path="${backup_directory}/${backup_file_name}"
+   	 
+	message "Restoring volumes '${volumes_to_restore}' from data container '${data_container}' with file '${backup_file_full_path}'"
+    docker run --rm  -v ${backup_directory}:/backup --volumes-from ${data_container} ${image_base} tar -xzvf /backup/${backup_file_name} ${volumes_to_restore}
+    
+    if [ ${?} -eq 0 ];then
+		message "Restore '${backup_file_full_path}' in ${data_container}"
+		meesage "Testing the backup .."    
+		docker run --rm  -v ${backup_directory}:/backup --volumes-from ${data_container} ${image_base} ls -lh ${volumes_to_restore}
+		return 0
+    else
+		message "ERROR! restoring '${backup_file_full_path}'"
+		return 1
+    fi
+}
